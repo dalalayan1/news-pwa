31fe454692f882e4c51dd3d7c5d3875189deb682
Ayan Dalal <ayan.dalal@olacabs.com>
2020-05-27 02:00:18 +0530

    initial news fetch

---
+++ app.js
@@ -0,0 +1,39 @@
+window.addEventListener('load', init);
+const API_KEY = "6321f37f7ee44de58d16f3c18a7ff614";
+function init() {
+    updateNews();
+}
+
+async function updateNews() {
+
+    try {
+        const res = await fetch(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${API_KEY}`);
+        const newsList = await res.json();
+        const newsWrapper = document.getElementById('newsWrapper');
+
+        try {
+            loadNews(newsList, newsWrapper);
+        } catch (error) {
+            newsWrapper.innerHTML = `<h1>Something went wrong!</h1>`;
+        }
+    } catch (error) {
+        console.log("Network error", error);
+        return {};
+    }
+}
+
+function loadNews(data, selector) {
+    selector.innerHTML = data.articles.map(addArticle).join('\n');
+}
+
+function addArticle(datum) {
+    return `
+        <div class='news-item'>
+            <a href='${datum.url}'>
+            <h2>${datum.title}</h2>
+            <img src="${datum.urlToImage}" alt="">
+            <p>${datum.description}</p>
+            </a>
+        </div>
+    `;
+}
